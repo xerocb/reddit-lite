ee6152d5a92b7ed536c51066ff5d1d02f0ab0af4
xerocb <ciaransbrady@gmail.com>
2025-02-20 15:12:47 +0000

    Add Comment styling

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,14 +1,14 @@
 import moment from "moment";
 import React from "react";
 import ReactMarkdown from 'react-markdown';
+import styles from './Comment.module.css';
 
 function Comment({ key, comment }) {
     return (
-        <>
-            <p>{comment.author}</p>
-            <p>{moment.unix(comment.created_utc).fromNow()}</p>
-            <ReactMarkdown children={comment.body} />
-        </>
+        <div className={styles.container}>
+            <p className={styles.meta}>u/{comment.author} - {moment.unix(comment.created_utc).fromNow()}</p>
+            <ReactMarkdown children={comment.body} className={styles.main} />
+        </div>
     );
 }
 
